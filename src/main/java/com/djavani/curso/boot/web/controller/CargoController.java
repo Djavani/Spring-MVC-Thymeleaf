357fa1314e18f1c5a7f3e2fdd4fd7964b66dbd29
djavani <djavanirgomes@gmail.com>
2018-06-23 22:26:31 -0300

    inserindo cargos e criando conversor de String para Ling para salvar Id do departamento

--- src/main/java/com/djavani/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/djavani/curso/boot/web/controller/CargoController.java
@@ -1,21 +1,50 @@
 package com.djavani.curso.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.djavani.curso.boot.domain.Cargo;
+import com.djavani.curso.boot.domain.Departamento;
+import com.djavani.curso.boot.service.CargoService;
+import com.djavani.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 	
+	@Autowired
+	private CargoService cargoService;
+	
+	@Autowired
+	private DepartamentoService departmentoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "cargo/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
 		return "cargo/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDeDepartamentos() {
+		return departmentoService.buscarTodos();
 	}
 
 }
