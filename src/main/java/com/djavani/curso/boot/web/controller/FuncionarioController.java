aa7a96e349dd69d551e8381357932742889d201f
djavani <djavanirgomes@gmail.com>
2018-06-24 23:18:13 -0300

    Editando e excluindo funcionários

--- src/main/java/com/djavani/curso/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/djavani/curso/boot/web/controller/FuncionarioController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -44,6 +45,29 @@ public class FuncionarioController {
 		attr.addFlashAttribute("success", "Funcionaroi inserido com sucesso.");
 		return "redirect:/funcionarios/cadastrar";
 	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("funcionario", funcionarioService.buscaPorId(id));		
+		return "funcionario/cadastro";
+	}
+	
+	
+	@PostMapping("/editar")
+	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.editar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr)	{
+		funcionarioService.exluir(id);
+		attr.addFlashAttribute("success", "Funcionário removido com sucesso.");
+		return "redirect:/funcionarios/listar";
+	}
+	
 	
 	@ModelAttribute("cargos")
 	public List<Cargo> getCargos() {
